6b60749b6fb0c662efccab8daffb040d9ac6d0ef
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-12 15:02:20 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,39 @@
+# a=int(input("enter your age : "))
+# b=int(input("enter your roll number : "))
+# print(a,b)
+
+# # a,b=(int(x) for x in input().split())
+# # print(a+b)
+
+# a=int(input("enter the value of a:"))
+# if (a==5):
+#     print("yes equal")
+# else:
+#     print("not equal")    
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):
+#     print("a is greater than b "+"a>b")
+# elif(a==b):
+#     print("a is equal to b ")
+# else:
+# #     print("a is less than b "+"a<b")     
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):print("a is great")
+# if(a<b):print("a is least")
+# if(a>=b):print("a is greater than equal to b")
+# if(a<=b):print("a is lesser than or equal to b")
+# if(a!=b):print("a is not equal to b")
+# if(a==b):print("a is equal to b")
+
+
+a,b=[int(x) for x in input().split()]
+if(a>b):print("a is greater")
+elif(a<b):print("a is least")
+elif(a>=b):print("a is greater than equal to b")
+elif(a<=b):print("a is lesser than or equal to b")
+elif(a!=b):print("a is not equal to b")
+elif(a==b):print("a is equal to b")
+
+
